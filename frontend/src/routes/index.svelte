414171b8f34d2c9a30915d49ce409e6f7483f4f1
Georg <Georg.Braun92@gmail.com>
2022-05-14 18:12:30 +0200

    ignore imports

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,12 +1,16 @@
 <script>
-	import AddEmployee from '../AddEmployee.svelte';
-	import Overview from '../Overview.svelte';
+    
+	//import AddEmployee from '../AddEmployee.svelte';
+	//import Overview from '../Overview.svelte';
+    
 </script>
 
 <div class="container" style="padding: 50px 0 100px 0;">
 	<h1>👨🏾‍💼👩🏻‍💼Office days?</h1>
-	<Overview />
+	<!--
+    <Overview />
 	<AddEmployee />
+    -->
 
 	<div style="margin-top: 30px;">
 		<b>A Slack Time Production 🚀</b>
