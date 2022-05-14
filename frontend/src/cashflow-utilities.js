cc6cbab9e3684f86ff3c02eff6f1d7413b1e8a59
Georg <Georg.Braun92@gmail.com>
2022-05-14 21:34:31 +0200

    remove unused files

--- frontend/src/cashflow-utilities.js
+++
@@ -1,9 +0,0 @@
-import { moneyMovementStore, categoryStore } from './store';
-import { get } from 'svelte/store';
-
-export function getCategoryName(id){
-    const categories = get(categoryStore);
-    
-    const result = categories.find(_ => _.id == id);
-    return result.name ?? "";
-}
