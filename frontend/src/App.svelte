d1c24ca55633d017c892499045a346fddd97cfac
Georg <Georg.Braun92@gmail.com>
2022-05-14 18:21:07 +0200

    show overview

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,15 @@
 <script>
     
 	//import AddEmployee from '../AddEmployee.svelte';
-	//import Overview from '../Overview.svelte';
+	import Overview from './Overview.svelte';
     
 </script>
 
 <div class="container" style="padding: 50px 0 100px 0;">
 	<h1>👨🏾‍💼👩🏻‍💼Office days?</h1>
-	<!--
+	
     <Overview />
+    <!--
 	<AddEmployee />
     -->
 
