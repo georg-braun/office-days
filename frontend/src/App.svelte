3a60c75cfa9d4e43944a732ded8157cf856d44a9
Georg Braun <5886884+georg-braun@users.noreply.github.com>
2022-05-18 13:10:23 +0000

    emoji

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="container" style="padding: 50px 0 100px 0;">
-	<h1>👨🏾‍💼👩🏻‍💼🐶🐈‍⬛🦍🐼Office days?</h1>
+	<h1>👨🏾‍💼 👩🏻‍💼 🐶 🐱 🐼 Office days?</h1>
 
 	<Overview />	
 	<AddEmployee />
